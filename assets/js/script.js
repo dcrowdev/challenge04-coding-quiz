0d3a1bd7344fde0533f8a22ab1a2ded2ede24509
Dylan Crowley <dcrowdev1025@gmail.com>
2022-10-03 17:56:16 -0400

    added array of objects with questions and answers

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+var questions = [
+{
+"question1": "Commonly used data types DO NOT include:",
+"1": "strings",
+"2": "booleans",
+"3": "alerts",
+"4": "numbers"
+}
+{
+"question2": "The condition in an if / else statement is enclosed within ____.",
+"1": "quotes",
+"2": "curly brackets",
+"3": "parentheses",
+"4": "square brackets"
+}
+{
+"question3": "Arrays in JavaScript can be used to store ____.",
+"1": "numbers and strings",
+"2": "other arrays",
+"3": "booleans",
+"4": "all of the above"
+}
+{
+"question4": "String values must be enclosed within ____ when being assigned to variables.",
+"1": "commas",
+"2": "curly brackets",
+"3": "quotes",
+"4": "parentheses"
+}
+{
+"question5": "A very usefool tool used during development and debugging for printing content to the debugger is:",
+"1": "JavaScript",
+"2": "terminal / bash",
+"3": "for loops",
+"4": "console.log"
+}
+];
