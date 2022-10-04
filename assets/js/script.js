d6ad398af0d6b98addfb79869f305cb85558bc72
Dylan Crowley <dcrowdev1025@gmail.com>
2022-10-04 09:04:40 -0400

    added initial functionality to clicking answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,56 +1,70 @@
 var questions = [
 {
 "question1": "Commonly used data types DO NOT include:",
-"a": "strings",
-"b": "booleans",
-"c": "alerts",
-"d": "numbers"
+"a": "1. strings",
+"b": "2. booleans",
+"c": "3. alerts",
+"d": "4. numbers",
+"answer": "c"
 },
 {
 "question2": "The condition in an if / else statement is enclosed within ____.",
-"a": "quotes",
-"b": "curly brackets",
-"c": "parentheses",
-"d": "square brackets"
+"a": "1. quotes",
+"b": "2. curly brackets",
+"c": "3. parentheses",
+"d": "4. square brackets",
+"answer": "c"
 },
 {
 "question3": "Arrays in JavaScript can be used to store ____.",
-"a": "numbers and strings",
-"b": "other arrays",
-"c": "booleans",
-"d": "all of the above"
+"a": "1. numbers and strings",
+"b": "2. other arrays",
+"c": "3. booleans",
+"d": "4. all of the above",
+"answer": "d"
 },
 {
 "question4": "String values must be enclosed within ____ when being assigned to variables.",
-"a": "commas",
-"b": "curly brackets",
-"c": "quotes",
-"d": "parentheses"
+"a": "1. commas",
+"b": "2. curly brackets",
+"c": "3. quotes",
+"d": "4. parentheses",
+"answer": "c"
 },
 {
 "question5": "A very usefool tool used during development and debugging for printing content to the debugger is:",
-"a": "JavaScript",
-"b": "terminal / bash",
-"c": "for loops",
-"d": "console.log"
+"a": "1. JavaScript",
+"b": "2. terminal / bash",
+"c": "3. for loops",
+"d": "4. console.log",
+"answer": "d"
 }
 ];
 
 var getQuestionTitles = document.getElementById("questionTitles");
 var getStartQuizBtn = document.getElementById("startQuizBtn");
-var getList = document.getElementById("list");
+var getList = document.getElementById("list1");
 var getFontPageP = document.getElementById("frontP");
-var getAnswer1 = document.getElementById("answer1");
-var getAnswer2 = document.getElementById("answer2");
-var getAnswer3 = document.getElementById("answer3");
-var getAnswer4 = document.getElementById("answer4");
+var getAnswer1 = document.getElementById("a");
+var getAnswer2 = document.getElementById("b");
+var getAnswer3 = document.getElementById("c");
+var getAnswer4 = document.getElementById("d");
+var currentQuestion = questions[0];
+
+
+
+
+
+
+
+
 
 // QUIZ FUNCTIONS
 function startQuestions() {
 getQuestionTitles.innerHTML = questions[0].question1;
-getStartQuizBtn.style.display = "none";
+getStartQuizBtn.classList.add("hide");
+getList.classList.replace("hide", "list");
 getFontPageP.remove();
-getList.style.display = "block";
 getAnswer1.innerHTML = questions[0].a;
 getAnswer2.innerHTML = questions[0].b;
 getAnswer3.innerHTML = questions[0].c;
@@ -59,6 +73,21 @@ getAnswer4.innerHTML = questions[0].d;
 
 
 
+function nextQuestion() {
+currentQuestion = questions[currentQuestion] + 1;
+getQuestionTitles.innerHTML = currentQuestion[0];
+}
+
+
+
+function answerClicked(e) {
+ if (currentQuestion.answer == e.target.getAttribute('id')) {
+  e.target.dataset.value = "true";
+}
+  if (e.target.dataset.value == "true") {
+    alert("correct!");
+    } else {alert("false");}
+}
 
 
 
@@ -82,3 +111,13 @@ function secondsInterval() {
     }
    }
 };
+
+
+
+// CALLBACKS
+getStartQuizBtn.addEventListener("click", timer);
+getStartQuizBtn.addEventListener("click", startQuestions);
+getAnswer1.addEventListener("click", answerClicked);
+getAnswer2.addEventListener("click", answerClicked);
+getAnswer3.addEventListener("click", answerClicked);
+getAnswer4.addEventListener("click", answerClicked);
